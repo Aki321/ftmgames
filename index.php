3d5f87c67c6e1c6293229610c65d08d58a5bcffc
Edo-rebuild <22edonbz@gmail.com>
2020-06-16 17:58:08 +0900

    Modify blog and slider

--- index.php
+++ index.php
@@ -4,22 +4,13 @@
 <head>
     <meta charset="utf-8">
     <title>Free Time games</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="css/slick.css">
     <link rel="stylesheet" type="text/css" href="css/slick-theme.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <script src="https://kit.fontawesome.com/f3dff3c59a.js" crossorigin="anonymous"></script>
-    <script src="js/jquery-3.5.1.min.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <script>
-        $(function () {
-            $(".headC").click(function () {
-                $(".top_menu").slideToggle();
-            });
-        });
-    </script>
-
 </head>
 
 <body>
@@ -50,36 +41,26 @@
     </header>
 
     <section id="slider">
-            <!--
-            <ul class="slick01">
-                <li><a href="./slidepuzzle/index.php"><img alt="slide_01" src="img/slidepuzzle15.PNG" /></a></li>
-                <li><a href="./flipcard/index.php"><img alt="slide_02" src="img/fripcads.PNG" /></a></li>
-                <li><img alt="slide_03" src="img/demo3.jpg" /></li>
-            </ul>
-            -->
-
-            <div id="example-2" class="carousel slide" data-ride="carousel" style="width:400px">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="img/slidepuzzle15.PNG" alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img src="img/slidepuzzle15.PNG" alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img src="img/slidepuzzle15.PNG" alt="Third slide">
-    </div>
-    <a class="carousel-control-prev" href="#example-2" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#example-2" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
-</div>
-
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <!-- スライドさせる画像の設定 -->
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <a href="./games/slidepuzzle/index.php"><img class="slideImg d-block" src="img/slidepuzzle15.PNG" alt="第1スライド"></a>
+                </div><!-- /.carousel-item -->
+                <div class="carousel-item">
+                    <a href="./games/flipcard/index.php"><img class="slideImg d-block" src="img/fripcads.PNG" alt="第2スライド"></a>
+                </div><!-- /.carousel-item -->
+            </div><!-- /.carousel-inner -->
+            <!-- スライドコントロールの設定 -->
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">前へ</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">次へ</span>
+            </a>
+        </div><!-- /.carousel -->
     </section>
 
     <div class="listable">
@@ -88,14 +69,11 @@
                 <span class="fas fa-gamepad icon"></span><span class="indexTxt"> Games</span>
             </div>
             <div class="row">
-                <div class="p-1 col-12 col-sm-6 col-md-4">
-                    <a href="./games/slidepuzzle/index.php"><p>スライドパズル⓯</p><img class="mx-auto d-block img-fluid" alt="slide_01" src="img/slidepuzzle15.PNG" /></a>
+                <div class="p-4 col-12 col-sm-6 col-md-4">
+                    <a href="./games/slidepuzzle/index.php"><img class="shadow-sm p-3 mb-5 bg-white rounded mx-auto d-block img-fluid" alt="slide_01" src="img/slidepuzzle15.PNG" /></a>
                 </div>
-                <div class="p-1 col-12 col-sm-6 col-md-4">
-                    <a href="./games/flipcard/index.php"><p>神経衰弱</p><img class="mx-auto d-block img-fluid" alt="slide_02" src="img/fripcads.PNG" /></a>
-                </div>
-                <div class="p-1 col-12 col-sm-6 col-md-4">
-                    <p>15puzzle</p><img class="mx-auto d-block img-fluid" alt="slide_03" src="img/demo3.jpg" />
+                <div class="p-4 col-12 col-sm-6 col-md-4">
+                    <a href="./games/flipcard/index.php"><img class="shadow-sm p-3 mb-5 bg-white rounded mx-auto d-block img-fluid" alt="slide_02" src="img/fripcads.PNG" /></a>
                 </div>
             </div>
         </div>
@@ -106,24 +84,42 @@
             <div class="wrapIcon">
                 <span class="fas fa-blog icon"></span><span class="indexTxt"> Blog</span>
             </div>
-            <div class="allblogs">
-                <div>
-                    <a href="./blog/jsBeginnersGuide.php"><p>JavaScript~入門~</p></a>
+            <div class="allblogs row mt-5">
+                <div class="col-12 col-md-6">
+                    <div class="row col-12 bg-white rounded p-3 pl-2 ml-auto mr-auto shadow-sm mb-2">
+                        <div class="p-2 col-3"><i class="fab fa-js-square"></i></div>
+                        <div class="col-9 mt-auto mb-auto"><a href="./blog/jsBeginnersGuide.php"><p class="h4 mb-0">JavaScript ~Hello World~</p></a></div>
+                    </div>
                 </div>
-                <div>
-                    <a href="./blog/jsArray.php"><p>JavaScript~配列~</p></a>
+                <div class="col-12 col-md-6">
+                    <div class="row col-12 bg-white rounded p-3 pl-2 ml-auto mr-auto shadow-sm mb-2">
+                        <div class="p-2 col-3"><i class="fab fa-js-square"></i></div>
+                        <div class="col-9 mt-auto mb-auto"><a href="./blog/jsArray.php"><p class="h4 mb-0">JavaScript ~配列~</p></a></div>
+                    </div>
                 </div>
-                <div>
-                    <a href="./blog/jsForeach.php"><p>JavaScript~forEach~</p></a>
+                <div class="col-12 col-md-6">
+                    <div class="row col-12 bg-white rounded p-3 pl-2 ml-auto mr-auto shadow-sm mb-2">
+                        <div class="p-2 col-3"><i class="fab fa-js-square"></i></div>
+                        <div class="col-9 mt-auto mb-auto"><a href="./blog/jsForeach.php"><p class="h4 mb-0">JavaScript ~forEach~</p></a></div>
+                    </div>
                 </div>
-                <div>
-                    <a href="./blog/jsFor.php"><p>JavaScript~for~</p></a>
+                <div class="col-12 col-md-6">
+                    <div class="row col-12 bg-white rounded p-3 pl-2 ml-auto mr-auto shadow-sm mb-2">
+                        <div class="p-2 col-3"><i class="fab fa-js-square"></i></div>
+                        <div class="col-9 mt-auto mb-auto"><a href="./blog/jsFor.php"><p class="h4 mb-0">JavaScript ~for~</p></a></div>
+                    </div>
                 </div>
-                <div>
-                    <a href="./blog/jsMapMethod.php"><p>JavaScript~mapメソッド~</p></a>
+                <div class="col-12 col-md-6">
+                    <div class="row col-12 bg-white rounded p-3 pl-2 ml-auto mr-auto shadow-sm mb-2">
+                        <div class="p-2 col-3"><i class="fab fa-js-square"></i></div>
+                        <div class="col-9 mt-auto mb-auto"><a href="./blog/jsMapMethod.php"><p class="h4 mb-0">JavaScript ~mapメソッド~</p></a></div>
+                    </div>
                 </div>
-                <div>
-                    <a href="./blog/jsMapObject.php"><p>JavaScript~Mapオブジェクト~</p></a>
+                <div class="col-12 col-md-6">
+                    <div class="row col-12 bg-white rounded p-3 pl-2 ml-auto mr-auto shadow-sm mb-2">
+                        <div class="p-2 col-3"><i class="fab fa-js-square"></i></div>
+                        <div class="col-9 mt-auto mb-auto"><a href="./blog/jsObject.php"><p class="h4 mb-0">JavaScript ~Mapオブジェクト~</p></a></div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -171,10 +167,9 @@
 
     </footer>
 
-
-
-    <!--<script type="text/javascript" src="js/slick.min.js"></script>
-    <script type="text/javascript" src="js/main.js"></script>-->
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 
 </body>
 
