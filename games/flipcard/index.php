adc573a97ba840798a911598129bfddbd7eded1f
Edo-rebuild <22edonbz@gmail.com>
2020-06-20 13:52:14 +0900

    Modify header

--- games/flipcard/index.php
+++ games/flipcard/index.php
@@ -17,18 +17,11 @@ if (!isset($_SESSION["visited"])) {
 <head>
     <meta charset="utf-8">
     <title>神経衰弱ゲーム</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../css/style.css">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <script src="https://kit.fontawesome.com/f3dff3c59a.js" crossorigin="anonymous"></script>
-    <script src="../js/jquery-3.5.1.min.js"></script>
-    <script>
-        $(function() {
-            $(".headC").click(function() {
-                $(".top_menu").slideToggle();
-            });
-        });
-    </script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+ 
 
     <script>
         'use strict';
@@ -145,34 +138,33 @@ if (!isset($_SESSION["visited"])) {
 </head>
 
 <body onload="init()">
-    <header>
-        <div class="container">
-            <div class="container-small">
-                <a href="./index.php" class="headA"><span class="fas fa-gamepad icon"></span>神経衰弱</a>
-
-                <button type="button" class="headC">
-                    <span class="fa fa-bars" title="MENU"></span>
-                </button>
-            </div>
-
-            <nav class="top_menu">
-                <ul>
-                    <li class="top">
-                        <a href="../index.php"><span class="fas fa-home"></span> Top</a>
-                    </li>
-                    <li class="games">
-                        <a href="../index.php"><span class="fas fa-gamepad"></span> Games</a>
-                    </li>
-                    <li class="about">
-                        <a href="../index.php"><span class="fas fa-book"></span> About</a>
-                    </li>
-                </ul>
-            </nav>
+<header>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top">
+        <a class="navbar-brand" href="./index.php"><span class="fas fa-gamepad"></span> 神経衰弱♠</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                <a class="nav-link" href="../../top/index.php"><span class="fas fa-home"></span></a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="../../top/index.php#listable"><span class="fas fa-gamepad"></span></a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="../../top/index.php#blogs"><span class="fas fa-blog icon"></span></a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="../../top/index.php#aboutcont"><span class="fas fa-book"></span></span></a>
+                </li>
+            </ul>
         </div>
-    </header>
+    </nav>
+</header>
 
     <main>
-        <div class="container">
+        <div class="container mt-5">
             <h2>
                 <?php
                 if (!isset($_SESSION["bestTime"])) {
@@ -197,6 +189,9 @@ if (!isset($_SESSION["visited"])) {
     </main>
     <footer>
     </footer>
+    <script src="../../js/lib/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </body>
 
 </html>
