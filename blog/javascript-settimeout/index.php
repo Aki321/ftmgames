b7d020998eff58f7b49486b9ea03d459fd38b309
Edo-rebuild <22edonbz@gmail.com>
2020-06-28 17:51:59 +0900

    Modify headline

--- blog/javascript-settimeout/index.php
+++ blog/javascript-settimeout/index.php
@@ -23,7 +23,7 @@ $title = "JavaScript setTimeout";
         </div>
         <p>本記事では、JavaScriptでsetTimeoutのサンプルプログラムを、ご紹介します。</p>
 
-        <h2>Windowのサイズを取得するサンプル</h2>
+        <h2>setTimeoutを使って、クリックした５秒後に、そのHTMLの要素を消すサンプル</h2>
         <h3>demo</h3>
         <div class=wrap_demo>
             <div id="moveBox"></div>
